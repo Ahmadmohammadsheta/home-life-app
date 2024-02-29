0fb5493dd412f7efb4e3226415e53d9bc3e914ab
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2024-02-29 22:36:39 +0300

    craete main tables

---
+++ app/Http/Controllers/MainProjectController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MainProject;
+use Illuminate\Http\Request;
+
+class MainProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return "index";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MainProject $mainProject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MainProject $mainProject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MainProject $mainProject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MainProject $mainProject)
+    {
+        //
+    }
+}
