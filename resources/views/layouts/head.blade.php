a9a121d944b9f3ba703f616e02914a060bfa13ab
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2024-04-27 14:58:40 +0300

    css js

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,4 +5,4 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-<link href="{{URL::asset('assets/my-css/style.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/css/style.css')}}" rel="stylesheet">
