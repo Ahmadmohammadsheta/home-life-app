f2a7646866fc17fa4fd1a14d4ed552ff4a4f2cf3
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2024-05-16 08:25:42 +0300

    Category testing success

--- tests/Feature/Category/CategotyTest.php
+++ tests/Feature/Category/CategotyTest.php
@@ -4,48 +4,45 @@ namespace Tests\Feature\Category;
 
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Type;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CategotyTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function data() {
+        // Create a new user
+        $user = User::factory()->create();
+
+        $type = Type::create(['name' => 'New Type']);
+
+        $category = Category::create([
+            'name' => 'New Category',
+            'is_parent' => '1',
+            'type_id' => $type->id
+        ]);
+
+        $updatedCategory = Category::create([
+            'name' => 'New Category 2',
+            'is_parent' => '0',
+            'type_id' => $type->id
+        ]);
+
+        return [
+            'user' => $user,
+            'type' => $type,
+            'category' => $category,
+            'updatedCategory' => $updatedCategory
+        ];
+    }
+
     /**
      * A basic feature test example.
      */
     public function test_index(): void
-    {
-        $response = $this->get('/categories');
-
-        $response->assertStatus(302);
-    }
-
-    public function test_show(): void
-    {
-        $response = $this->get('/categories/1');
-
-        // $response->assertViewIs('crud.show');
-        $response->assertStatus(302);
-    }
-
-    public function test_create(): void
-    {
-        // Create a new user
-        $user = User::factory()->create();
-
-        // Login the user
-        // $this->actingAs($user);
-
-
-        $response = $this->get('/categories/create');
-
-        $response->assertStatus(302);
-
-        // $response->assertSee('Done');
-        // $response->assertViewIs('crud.create');
-    }
-
-    public function test_store(): void
     {
         // Create a new user
         $user = User::factory()->create();
@@ -53,22 +50,73 @@ class CategotyTest extends TestCase
         // Login the user
         $this->actingAs($user);
 
+        $response = $this->get('/categories');
+
+        $response->assertSee($user->name); // done
+    }
+
+    public function test_show(): void
+    {
+        // Login the user
+        $this->actingAs($this->data()['user']);
+
+        $response = $this->get('/categories/'. $this->data()['category']->id);
+
+        $response->assertSee('Category');
+    }
+
+    public function test_create(): void
+    {
+        // Login the user
+        $this->actingAs($this->data()['user']);
+
+
+        $response = $this->get('categories/create');
+
+        $response->assertSee('Categories');
+    }
+
+    public function test_store(): void
+    {
+        // Login the user
+        $this->actingAs($this->data()['user']);
+
         // Send a POST request to the create category route
         $response = $this->post(route('categories.store'), [
-            'name' => 'New Category',
-            'is_parent' => '1',
-            'type_id' => '1'
+            $this->data()['category']
         ]);
 
         // Assert that the response is a redirect
-        $response->assertStatus(200);
+        $response->assertStatus(302);
         $response->assertRedirect();
 
         // Assert that the category was created
         $this->assertDatabaseHas('categories', [
             'name' => 'New Category',
             'is_parent' => '1',
-            'type_id' => '1'
+            'type_id' => $this->data()['type']->id
+        ]);
+    }
+
+    public function test_update(): void
+    {
+        // Login the user
+        $this->actingAs($this->data()['user']);
+
+        // Send a POST request to the create category route
+        $response = $this->put(route('categories.update', $this->data()['category']->id), [
+            $this->data()['updatedCategory']
+        ]);
+
+        // Assert that the response is a redirect
+        $response->assertStatus(302);
+        $response->assertRedirect();
+
+        // Assert that the category was created
+        $this->assertDatabaseHas('categories', [
+            'name' => 'New Category 2',
+            'is_parent' => '0',
+            'type_id' => $this->data()['type']->id
         ]);
     }
 }
